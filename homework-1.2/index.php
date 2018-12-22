8b5a1e6dc7b0aa1785cb8b460a52c4c671b65ab7
Konstantin Pronin <troyandex@gmail.com>
2018-12-22 22:01:00 +0400

    fix: fantastic animals / php pos in html / add task

--- homework-1.2/index.php
+++ homework-1.2/index.php
@@ -7,48 +7,7 @@
 	    'Eurasia' => ['Pteromys volans', 'Bison bonasus', 'Apodemus agrarius'],
 	    'North America' => ['Mammuthus columbi', 'Bison bison'],
 	    'South America' => ['Eunectes murinus']
-    ];
-    // 1-----------------------------------------------------------------------
-    echo '<h1>Пример выполнения задания "Жестокое обращение с животными"</h1>';
-	echo '<h2>1. Исходный массив:</h2>';
-	echo "<details><pre>";
-	print_r ($continents); //  выводит первоначальный масив 
-	foreach ($continents as $continents_key => $animals)
-	{
-	    $continent[] = $continents_key;
-	    echo "<h3>$continents_key</h3>";
-	    echo implode(", ", $animals); // заменил цикл на implode
-	    foreach ($animals as $two) // подготовка для двойных названий
-	    {
-	        $two_words = explode(" ", $two);
-	        if (count($two_words) == 2) 
-	        {
-	            $double_name[] = $two;
-	            $first_world[] = $two_words[0];
-	            $second_word[] = $two_words[1];
-	        }
-	    }
-    }
-    echo "</pre></details>";
-    // 2-----------------------------------------------------------------------
-    echo '<hr>';
-	echo '<h2>2. Названия, состоящие из двух слов:</h2>';
-	foreach ($double_name as $item) 
-	{
-	    echo "$item<br>";
-	}
-	echo '<hr>';
-    shuffle($second_word);
-    
-    // 3-----------------------------------------------------------------------
-	echo '<h2>3. "Фантазийные" названия:</h2>';
-	foreach ($second_word as $last)
-	{
-	    $title = array_shift($continent);
-	    $first = array_shift($first_world);
-	    echo "<h3>$title</h3>";
-	    echo $first . ' ' . $last . '<br>';
-	}
+	];
 ?>
 
 <!DOCTYPE html>
@@ -63,7 +22,59 @@
   	hr { margin-top: 1em; }
   </style>
 </head>
-<body>	
-    
+<body>
+
+<?php
+    // 1-----------------------------------------------------------------------
+    echo '<h1>Пример выполнения задания "Жестокое обращение с животными"</h1>';
+	echo '<h2>1. Исходный массив:</h2>';
+	echo "<details><pre>";
+	// print_r ($continents); //  выводит первоначальный масив 
+	foreach ($continents as $continents_key => $animals)
+	{
+	    $continent[] = $continents_key;
+	    echo "<h3>$continents_key</h3>";
+	    echo implode(", ", $animals); // заменил цикл на implode
+	    foreach ($animals as $two) // подготовка для двойных названий
+	    {
+	        $two_words = explode(" ", $two);
+	        if (count($two_words) == 2) 
+	        {
+	            $double_name[] = $two;
+	            $first_world[] = $two_words[0];
+				$second_word[] = $two_words[1];
+				$continentFantasy[] = $continents_key;
+	        }
+	    }
+    }
+    echo "</pre></details>";
+    // 2-----------------------------------------------------------------------
+    echo '<hr>';
+	echo '<h2>2. Названия, состоящие из двух слов:</h2>';
+	echo implode(", ", $double_name); // заменил цикл на implode
+	echo '<hr>';
+    // 3-----------------------------------------------------------------------
+	echo '<h2>3. "Фантазийные" названия:</h2>';
+	shuffle($second_word);
+	echo implode(", ", $continent); // заменил цикл на implode
+	echo '<hr>';
+	echo '<h2>Дополнительное задание:</h2>';
+	$tempTitle = null;
+	foreach ($second_word as $last)
+	{
+		$title = array_shift($continentFantasy); // выгружаем континет
+		$first = array_shift($first_world); // и первое слово
+		if ($tempTitle !== $title) // если новый континент 
+		{
+			$tempTitle = $title;
+			echo "<h3>$title</h3>";
+			echo $first . ' ' . $last;
+		}
+		else // если как предидущий континент
+		{
+			echo ', ' . $first . ' ' . $last;
+		}
+	}
+?>
 </body>
 </html>
